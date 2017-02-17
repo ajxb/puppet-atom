facb4768f41664be2f15dfc9b94bf8485d8ee18f
Adam Butler <adam.j.butler@gmail.com>
2017-02-17 14:20:47 +0000

    Initial stab at tests for the atom class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,37 @@
 require 'spec_helper'
 describe 'atom' do
-  context 'with default values for all parameters' do
+
+  context 'On Ubuntu OS with default parameters' do
+    let :facts do
+      {
+        :lsbdistcodename => 'xenial',
+        :lsbdistid       => 'Ubuntu',
+        :lsbdistrelease  => '16.04',
+        :operatingsystem => 'Ubuntu',
+        :osfamily        => 'Debian',
+        :puppetversion   => Puppet.version,
+      }
+    end
+
+    it { is_expected.to compile.with_all_deps }
     it { should contain_class('atom') }
+    it { should contain_class('atom::config') }
+    it { should contain_class('atom::install') }
+    it { should contain_class('atom::params') }
+    it { is_expected.to contain_class('atom::install').that_comes_before('Class[atom::config]') }
+  end
+
+  context 'With unsupported operatingsystem' do
+    let :facts do
+      {
+        :operatingsystem => 'Unsupported OS',
+      }
+    end
+
+    it {
+      expect {
+        subject.call
+      }.to raise_error(Puppet::Error, /Unsupported OS not supported/)
+    }
   end
 end
