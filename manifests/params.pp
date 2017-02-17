849adadfc9143b0ee65f819d2619674357c4d745
Adam Butler <adam.j.butler@gmail.com>
2017-02-17 14:20:22 +0000

    Update params to use new facts hash

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,13 @@
 class atom::params {
 
-  case $::operatingsystem {
+  case $facts['operatingsystem'] {
     'Ubuntu': {
       $package_ensure = 'latest'
       $package_name   = 'atom'
       $disable_gpu    = false
     }
     default: {
-      fail("${::operatingsystem} not supported")
+      fail("${facts['operatingsystem']} not supported")
     }
   }
 }
